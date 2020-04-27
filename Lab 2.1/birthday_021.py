dbf1f6f777303b76868e780b5593bcd6076be27f
Qiyang Zuo <56083302+Alpha-yoyo@users.noreply.github.com>
2020-04-27 18:51:22 +0100

    Create birthday_021.py

---
+++ Lab 2.1/birthday_021.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import calendar
+import sys
+
+
+def main():
+   day = int(sys.argv[1])
+   month = int(sys.argv[2])
+   year = int(sys.argv[3])
+   weekday = calendar.weekday(year, month, day)
+   sentences = {
+      0: "You were born on a Monday and Monday's child is fair of face.",
+      1: "You were born on a Tuesday and Tuesday's child is full of grace.",
+      2: "You were born on a Wednesday and Wednesday's child is full of woe.",
+      3: "You were born on a Thursday and Thursday's child has far to go.",
+      4: "You were born on a Friday and Friday's child is loving and giving.",
+      5: "You were born on a Saturday and Saturday's child works hard for a living.",
+      6: "You were born on a Sunday and Sunday's child is fair and wise and good in every way."
+   }
+
+   print(sentences[weekday])
+
+
+if __name__ == "__main__":
+   main()
